0a04e55c1c2c3011cf6cbb843a7859c1df28b7af
CMerlin <1079843691@qq.com>
2017-11-01 16:58:35 +0800

    1.调试录制功能

--- ffmpegDemo/muxing.c
+++ ffmpegDemo/muxing.c
@@ -43,9 +43,13 @@
 #include <libswscale/swscale.h>
 #include <libswresample/swresample.h>
 
-#define STREAM_DURATION   10.0
+#define STREAM_DURATION   10.0 /* 控制录制的文件的长度 */
+//#define STREAM_DURATION   10.0
+
 #define STREAM_FRAME_RATE 25 /* 25 images/s */
+//#define STREAM_PIX_FMT    AV_PIX_FMT_YUV422P; //AV_PIX_FMT_YUV420P /* default pix_fmt */
 #define STREAM_PIX_FMT    AV_PIX_FMT_YUV420P /* default pix_fmt */
+
 
 #define SCALE_FLAGS SWS_BICUBIC
 
@@ -66,6 +70,7 @@ typedef struct _CodeAAttr{
 	int height;
 	int bit_rate; /*码率*/
 	int sample_rate; /*采样率*/
+	int codec_id; /*编码类型*/
 } CODEA_ATTR, *P_CODEA_ATTR; 
 
 #endif
@@ -94,7 +99,11 @@ typedef struct OutputStream {
 static void log_packet(const AVFormatContext *fmt_ctx, const AVPacket *pkt)
 {
     AVRational *time_base = &fmt_ctx->streams[pkt->stream_index]->time_base;
-
+#if 0 /*只显示音频数据 OR 只显示视频数据*/
+	if(1 == pkt->stream_index){
+		return;
+	}
+#endif
     printf("pts:%s pts_time:%s dts:%s dts_time:%s duration:%s duration_time:%s stream_index:%d\n",
            av_ts2str(pkt->pts), av_ts2timestr(pkt->pts, time_base),
            av_ts2str(pkt->dts), av_ts2timestr(pkt->dts, time_base),
@@ -127,9 +136,11 @@ int initCodeAttr(CODEV_ATTR *videoAttr, CODEA_ATTR *audioAttr){
 	videoAttr->bit_rate = 400000;
 	videoAttr->width    = 352;
     videoAttr->height   = 288;
-	videoAttr->frame_rate = 15;
-	videoAttr->width    = 720;
-    videoAttr->height   = 480;
+	videoAttr->frame_rate = 25;
+	//videoAttr->width    = 1920; //720;
+    //videoAttr->height   =1080; //480;
+    //videoAttr->width    = 720;
+    //videoAttr->height   = 480;
 	/*音频的*/
 	audioAttr->bit_rate	= 64000;
 	audioAttr->sample_rate = 44100;
@@ -248,6 +259,55 @@ int setCodeAttrA(AVCodecContext *c, CODEA_ATTR* pattr, AVCodec *codec)
 #endif
 }
 
+/*******************************************************************************************
+* Description:创建流通道
+* Input:oc
+* Output:st
+* Return:0-成功 0<-失败
+******************************************************************************************/
+int add_stream_to_file(AVFormatContext *oc, AVStream **st){
+#if 1
+	/* 创建流通道 */
+    *st = avformat_new_stream(oc, NULL);
+    if (!(*st)) {
+        fprintf(stderr, "Could not allocate stream\n");
+		return -1;
+    }
+    (*st)->id = oc->nb_streams-1;
+#endif
+	return 0;
+}
+
+int setCodeAttr(AVCodecContext *enc, OutputStream *ost, AVCodec **codec, CODEV_ATTR *videoAttr, CODEA_ATTR *audioAttr)
+{
+#if 0
+	CODEV_ATTR videoAttr = {0};
+	CODEA_ATTR audioAttr = {0};
+	initCodeAttr(&videoAttr, &audioAttr);
+#endif
+	AVCodecContext *c;
+	//c = ost->enc;
+	c = enc;
+
+	switch ((*codec)->type) {
+	case AVMEDIA_TYPE_VIDEO:
+		{
+			ost->st->time_base = (AVRational){ 1, (videoAttr->frame_rate) };
+			setCodeAttrV(c, videoAttr);
+			break;
+		}
+	case AVMEDIA_TYPE_AUDIO:
+		{
+			ost->st->time_base = (AVRational){ 1, (audioAttr->sample_rate) };
+			setCodeAttrA(c, audioAttr, *codec);
+			break;
+		}
+	default:
+		break;
+	}
+
+	return 0;
+}
 
 /***************************************************************************************
 * Description:添加一个输出流
@@ -270,21 +330,24 @@ static void add_stream(OutputStream *ost, AVFormatContext *oc,
         exit(1);
     }
 
+#if 0
 	/* 创建流通道 */
     ost->st = avformat_new_stream(oc, NULL);
     if (!ost->st) {
         fprintf(stderr, "Could not allocate stream\n");
         exit(1);
     }
-	/* 解码网络流 */
     ost->st->id = oc->nb_streams-1;
-    c = avcodec_alloc_context3(*codec);
+#endif
+	//add_stream_to_file(oc, &(ost->st));
+	/* 解码网络流 */
+	c = avcodec_alloc_context3(*codec);
     if (!c) {
         fprintf(stderr, "Could not alloc an encoding context\n");
         exit(1);
     }
 	ost->enc = c;
-#if 1
+#if 0
 	CODEV_ATTR videoAttr = {0};
 	CODEA_ATTR audioAttr = {0};
 	initCodeAttr(&videoAttr, &audioAttr);
@@ -478,9 +541,10 @@ static AVFrame *get_audio_frame(OutputStream *ost)
     int16_t *q = (int16_t*)frame->data[0];
 
     /* check if we want to generate more frames */
-    if (av_compare_ts(ost->next_pts, ost->enc->time_base,
-                      STREAM_DURATION, (AVRational){ 1, 1 }) >= 0)
-        return NULL;
+    if (av_compare_ts(ost->next_pts, ost->enc->time_base, STREAM_DURATION, (AVRational){ 1, 1 }) >= 0){
+		//printf("[%s]:dont't new data frames! line:%d\n", __func__, __LINE__);
+		return NULL;
+	}
 
     for (j = 0; j <frame->nb_samples; j++) {
         v = (int)(sin(ost->t) * 10000);
@@ -500,6 +564,10 @@ static AVFrame *get_audio_frame(OutputStream *ost)
  * encode one audio frame and send it to the muxer
  * return 1 when encoding is finished, 0 otherwise
  */
+/****************************************************************************
+* Description:将媒体数据写入到录制文件中
+* Return：1-成功 0-获取YUV数据失败或是编码YUV数据失败
+******************************************************************************/
 static int write_audio_frame(AVFormatContext *oc, OutputStream *ost)
 {
     AVCodecContext *c;
@@ -526,14 +594,17 @@ static int write_audio_frame(AVFormatContext *oc, OutputStream *ost)
          * make sure we do not overwrite it here
          */
         ret = av_frame_make_writable(ost->frame);
-        if (ret < 0)
-            exit(1);
+        if (ret < 0){
+			printf("[%s][Error]:line:%d\n", __func__, __LINE__);
+			exit(1);
+		}
 
         /* convert to destination format */
         ret = swr_convert(ost->swr_ctx,
                           ost->frame->data, dst_nb_samples,
                           (const uint8_t **)frame->data, frame->nb_samples);
         if (ret < 0) {
+			printf("Error while converting\n");
             fprintf(stderr, "Error while converting\n");
             exit(1);
         }
@@ -548,6 +619,7 @@ static int write_audio_frame(AVFormatContext *oc, OutputStream *ost)
 #endif
     ret = avcodec_encode_audio2(c, &pkt, frame, &got_packet);
     if (ret < 0) {
+		printf("[Error]:Error encoding audio frame: %s\n", av_err2str(ret));
         fprintf(stderr, "Error encoding audio frame: %s\n", av_err2str(ret));
         exit(1);
     }
@@ -555,8 +627,8 @@ static int write_audio_frame(AVFormatContext *oc, OutputStream *ost)
     if (got_packet) {
         ret = write_frame(oc, &c->time_base, ost->st, &pkt);
         if (ret < 0) {
-            fprintf(stderr, "Error while writing audio frame: %s\n",
-                    av_err2str(ret));
+			printf("Error while writing audio frame: %s\n", av_err2str(ret));
+            fprintf(stderr, "Error while writing audio frame: %s\n", av_err2str(ret));
             exit(1);
         }
     }
@@ -637,7 +709,11 @@ static void open_video(AVFormatContext *oc, AVCodec *codec, OutputStream *ost, A
     }
 }
 
-/* Prepare a dummy image. */
+/************************************************************************
+* Description:生成一帧数YUV数据
+* Input:width height-生成的YUV数据的大小
+* Output:pict-生长的YUV的数据
+**************************************************************************/
 static void fill_yuv_image(AVFrame *pict, int frame_index,
                            int width, int height)
 {
@@ -659,6 +735,30 @@ static void fill_yuv_image(AVFrame *pict, int frame_index,
     }
 }
 
+/*******************************************************************************
+ * Description:将其其它类型的YUV数据转化成YUV420的数据
+ * Input:pix_fmt-源YUV数据的类型
+* Input:width hight-转化后YUV数据的大小
+* Return：0-成功 -1-失败
+*******************************************************************************/
+static int conversionYUV420(OutputStream *ost, enum AVPixelFormat pix_fmt, int width, int height){
+#if 1
+	/* as we only generate a YUV420P picture, we must convert it
+	 * to the codec pixel format if needed */
+        if (!ost->sws_ctx) {
+			printf("[%s]:null line:%d\n", __func__, __LINE__);
+            ost->sws_ctx = sws_getContext(width, height,AV_PIX_FMT_YUV420P, width, height, pix_fmt, SCALE_FLAGS, NULL, NULL, NULL);
+            if (!ost->sws_ctx) {
+                fprintf(stderr, "[Error]:Could not initialize the conversion context\n");
+                exit(1);
+            }
+        }
+        fill_yuv_image(ost->tmp_frame, ost->next_pts, width, height);
+        sws_scale(ost->sws_ctx, (const uint8_t * const *)ost->tmp_frame->data, ost->tmp_frame->linesize, 0, height, ost->frame->data, ost->frame->linesize);
+#endif
+	return 0;
+}
+
 /*********************************************************************************
 * Description:从yuv输入流中获取一帧YUV数据
 * Input ost：YUV流
@@ -668,19 +768,24 @@ static AVFrame *get_video_frame(OutputStream *ost)
 {
     AVCodecContext *c = ost->enc;
 	
-    /* check if we want to generate more frames */
-    if (av_compare_ts(ost->next_pts, c->time_base,
-                      STREAM_DURATION, (AVRational){ 1, 1 }) >= 0)
-        return NULL;
+    /* 检查是否到最大录制时长了，到了的话就结束录制 */
+    if (av_compare_ts(ost->next_pts, c->time_base, STREAM_DURATION, (AVRational){ 1, 1 }) >= 0){
+		//printf("[%s]:dont't new data frames! line:%d\n", __func__, __LINE__);
+		return NULL;
+	}
 
     /* when we pass a frame to the encoder, it may keep a reference to it
      * internally; make sure we do not overwrite it here */
-    if (av_frame_make_writable(ost->frame) < 0)
-        exit(1);
+    if (av_frame_make_writable(ost->frame) < 0){
+		printf("[%s][Error]:line:%d\n", __func__, __LINE__);
+		 exit(1);
+	} 
 
 	/* 生成特定格式的YUV数据给编码器 */
     if (c->pix_fmt != AV_PIX_FMT_YUV420P) {
-        /* as we only generate a YUV420P picture, we must convert it
+		conversionYUV420(ost, (c->pix_fmt), (c->width), (c->height));
+#if 0
+	/* as we only generate a YUV420P picture, we must convert it
          * to the codec pixel format if needed */
         if (!ost->sws_ctx) {
             ost->sws_ctx = sws_getContext(c->width, c->height,
@@ -689,6 +794,7 @@ static AVFrame *get_video_frame(OutputStream *ost)
                                           c->pix_fmt,
                                           SCALE_FLAGS, NULL, NULL, NULL);
             if (!ost->sws_ctx) {
+				printf("[Error]:Could not initialize the conversion context\n");
                 fprintf(stderr, "[Error]:Could not initialize the conversion context\n");
                 exit(1);
             }
@@ -697,6 +803,7 @@ static AVFrame *get_video_frame(OutputStream *ost)
         sws_scale(ost->sws_ctx,
                   (const uint8_t * const *)ost->tmp_frame->data, ost->tmp_frame->linesize,
                   0, c->height, ost->frame->data, ost->frame->linesize);
+#endif
     } else {
         fill_yuv_image(ost->frame, ost->next_pts, c->width, c->height);
     }
@@ -715,7 +822,7 @@ static AVFrame *get_video_frame(OutputStream *ost)
 * Description:
 * Input oc:输出的设备的文件描述符（可以简单的看成一个视频文件描述符）
 * Input ost:YUV数据流文件(可以看成流文件描述符)
-* 0-成功 1-失败
+* Return：1-成功 0-获取YUV数据失败或是编码YUV数据失败
 ***************************************************************************************************/
 static int write_video_frame(AVFormatContext *oc, OutputStream *ost)
 {
@@ -796,6 +903,128 @@ int openOutFile(AVFormatContext **oc, const char *filePath)
 
 #endif
 
+/*************************************************************************************
+* Description:初始化视频编码器
+* Output:
+***********************************************************************************/
+int initCodeVide(AVCodecContext **enc, AVFormatContext *oc,
+                       AVCodec **codec, CODEV_ATTR *pattr, OutputStream *ost){
+#if 1
+	AVCodecContext *c;
+    int i;
+	
+    /* 查找编码器 */
+	printf("[%s]:codec_id=%d line:%d\n", __func__, (pattr->codec_id), __LINE__);
+    *codec = avcodec_find_encoder(pattr->codec_id);
+    if (!(*codec)) {
+        fprintf(stderr, "[%s][Error]:Could not find encoder for '%s' line:%d\n", __func__, avcodec_get_name(pattr->codec_id), __LINE__);
+        return -1;
+    }
+	//add_stream_to_file(oc, &(ost->st));
+	/* 解码网络流 */
+	c = avcodec_alloc_context3(*codec);
+	 if (!c) {
+	//(*enc) = avcodec_alloc_context3(*codec);
+    //if (!(*enc)) {
+        fprintf(stderr, "Could not alloc an encoding context\n");
+		return -1;
+    }
+	 ost->enc = c;
+	//(enc) = c;
+	//(*enc) = c;
+	/*设置编码器的属性*/
+	if(AVMEDIA_TYPE_VIDEO != ((*codec)->type)){
+		return -1; /* 如果不是视频编码器，不进行设置*/
+	}
+	//printf("[%s]:line:%d\n", __func__, __LINE__);
+	setCodeAttrV(c, pattr);
+	/* Some formats want stream headers to be separate. */
+    if (oc->oformat->flags & AVFMT_GLOBALHEADER){
+		c->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+	}
+	//printf("[%s]:line:%d\n", __func__, __LINE__);
+#endif
+
+#if 0
+/* 解码网络流 */
+	c = avcodec_alloc_context3(*codec);
+    if (!c) {
+        fprintf(stderr, "Could not alloc an encoding context\n");
+        exit(1);
+    }
+	ost->enc = c;
+#endif
+
+#if 0
+	/* 添加音视频流，并默认的格式进行编码 */
+    if (fmt->video_codec != AV_CODEC_ID_NONE) {
+		//fmt->video_codec = AV_CODEC_ID_H264; /*H264编码方式*/
+        add_stream(&video_st, oc, &video_codec, fmt->video_codec);
+        have_video = 1;
+        encode_video = 1;
+    }
+
+	/* 打开音视频编码器，并设置缓存空间 */
+    if (have_video)
+        open_video(oc, video_codec, &video_st, opt);
+#endif
+	return 0;
+}
+
+/*************************************************************************************
+ * Description:初始化音频编码器
+ ***********************************************************************************/
+int initCodeAudio(AVCodecContext **enc, AVFormatContext *oc,
+		AVCodec **codec, CODEA_ATTR *pattr, OutputStream *ost){
+#if 1
+	AVCodecContext *c;
+	int i;
+
+	/* 查找编码器 */
+	*codec = avcodec_find_encoder(pattr->codec_id);
+	if (!(*codec)) {
+		fprintf(stderr, "[%s][Error]:Could not find encoder for '%s'\n", __func__, avcodec_get_name(pattr->codec_id));
+		return -1;
+	}
+	//add_stream_to_file(oc, &(ost->st));
+	/* 解码网络流 */
+	c = avcodec_alloc_context3(*codec);
+	 if (!c) {
+	//(*enc) = avcodec_alloc_context3(*codec);
+    //if (!(*enc)) {
+        fprintf(stderr, "Could not alloc an encoding context\n");
+		return -1;
+    }
+	ost->enc = c;
+	//(enc) = &c;
+	//(*enc) = c;
+	/*设置编码器的属性*/
+	if(AVMEDIA_TYPE_AUDIO != ((*codec)->type)){
+		return -1; /* 如果不是视频编码器，不进行设置*/
+	}
+	setCodeAttrA(c, pattr, (*codec));
+	/* Some formats want stream headers to be separate. */
+	if (oc->oformat->flags & AVFMT_GLOBALHEADER){
+		c->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+	}
+#endif
+
+#if 0
+if (fmt->audio_codec != AV_CODEC_ID_NONE) {
+	   add_stream(&audio_st, oc, &audio_codec, fmt->audio_codec);
+	   have_audio = 1;
+	   encode_audio = 1;
+   }
+
+#endif
+	return 0;
+}
+
+/*************************************************************************************
+*
+***********************************************************************************/
+
+
 
 /**************************************************************/
 /* media file output */
@@ -806,12 +1035,14 @@ int main(int argc, char **argv)
     AVOutputFormat *fmt;
     AVFormatContext *oc;
     AVCodec *audio_codec, *video_codec;
-    int ret;
+    int ret = 0, i = 0;
     int have_video = 0, have_audio = 0;
     int encode_video = 0, encode_audio = 0;
     AVDictionary *opt = NULL;
-    int i;
+    CODEV_ATTR videoAttr = {0};
+	CODEA_ATTR audioAttr = {0};
 
+	initCodeAttr(&videoAttr, &audioAttr);
     /* Initialize libavcodec, and register all codecs and formats. */
     av_register_all();
 
@@ -836,7 +1067,7 @@ int main(int argc, char **argv)
     }
 #endif
 
-#if 1
+#if 0
     /* 创建输出媒体的 context */
     avformat_alloc_output_context2(&oc, NULL, NULL, filename);
     if (!oc) {
@@ -846,25 +1077,58 @@ int main(int argc, char **argv)
     if (!oc)
         return 1;
 #endif
+	/* 输出文件相关的信息 */
 	if(0 > openOutFile(&oc, filename)){
 		return 1;
 	}
     fmt = oc->oformat;
+	videoAttr.codec_id = fmt->video_codec;
+	audioAttr.codec_id = fmt->audio_codec;
 
+#if 1
 	/* 添加音视频流，并默认的格式进行编码 */
     /* Add the audio and video streams using the default format codecs
      * and initialize the codecs. */
     if (fmt->video_codec != AV_CODEC_ID_NONE) {
-		fmt->video_codec = AV_CODEC_ID_H264; /*H264编码方式*/
+		//fmt->video_codec = AV_CODEC_ID_H264; /*H264编码方式*/
+		add_stream_to_file(oc, &(video_st.st));
         add_stream(&video_st, oc, &video_codec, fmt->video_codec);
         have_video = 1;
         encode_video = 1;
     }
     if (fmt->audio_codec != AV_CODEC_ID_NONE) {
+		add_stream_to_file(oc, &(audio_st.st));
         add_stream(&audio_st, oc, &audio_codec, fmt->audio_codec);
         have_audio = 1;
         encode_audio = 1;
     }
+	setCodeAttr((video_st.enc), &video_st, (&video_codec), &videoAttr,  &audioAttr);
+	setCodeAttr((audio_st.enc), &audio_st, (&audio_codec), &videoAttr, &audioAttr);
+#endif
+
+#if 0
+	/* 音视频编码相关的信息 */
+	initCodeAttr(&videoAttr, &audioAttr);
+	videoAttr.codec_id = fmt->video_codec;
+	audioAttr.codec_id = fmt->audio_codec;
+	/*编码器初始化*/
+	add_stream_to_file(oc, &(video_st.st));
+	initCodeVide(&(video_st.enc), oc, &(video_codec), &videoAttr, &video_st);
+	printf("[%s]:line:%d\n", __func__, __LINE__);
+	//video_st.st->time_base = video_st.enc->time_base; /*时间戳*/
+	video_st.st->time_base = (AVRational){ 1, (videoAttr.frame_rate) };
+	//printf("[%s]:line:%d\n", __func__, __LINE__);
+	
+
+	add_stream_to_file(oc, &(audio_st.st));
+	initCodeAudio(&(audio_st.enc), oc, &(audio_codec), &audioAttr, &audio_st);
+	//audio_st.st->time_base = audio_st.enc->time_base; /*时间戳*/
+	audio_st.st->time_base = (AVRational){ 1, (audioAttr.sample_rate) };
+	printf("[%s]:line:%d\n", __func__, __LINE__);
+	/*打开编码器*/
+	setCodeAttr(&video_st, (&video_codec));
+	setCodeAttr(&audio_st, (&audio_codec));
+#endif
 
 	/* 打开音视频编码器，并设置缓存空间 */
     /* Now that all the parameters are set, we can open the audio and
@@ -875,8 +1139,10 @@ int main(int argc, char **argv)
     if (have_audio)
         open_audio(oc, audio_codec, &audio_st, opt);
 
+
     av_dump_format(oc, 0, filename, 1);
-# if 0
+	printf("[%s]:line:%d\n", __func__, __LINE__);
+#if 0
 	/* 打开输出文件 */
     /* open the output file, if needed */
     if (!(fmt->flags & AVFMT_NOFILE)) {
@@ -896,7 +1162,9 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    while (encode_video || encode_audio) {
+	printf("[%s]:recording begin! line:%d\n", __func__, __LINE__);
+	/* 当写视频和音频数据都失败的时候，停止录制 */
+	while (encode_video || encode_audio) {
         /* select the stream to encode */
         if (encode_video &&
             (!encode_audio || av_compare_ts(video_st.next_pts, video_st.enc->time_base,
@@ -905,8 +1173,10 @@ int main(int argc, char **argv)
         } else {
             encode_audio = !write_audio_frame(oc, &audio_st); /* 写音频数据 */
         }
+		//printf("[%s]audio=%d video=%d line:%d\n", __func__, encode_audio, encode_video, __LINE__);
     }
-
+	printf("[%s]:recording over! line:%d\n", __func__, __LINE__);
+	
 	/* 写文件的尾 */
     /* Write the trailer, if any. The trailer must be written before you
      * close the CodecContexts open when you wrote the header; otherwise
